5d2217a176f9afdd2e8b5689069d209f722b2fbb
Edwin <edwin@welllinkhk.com>
2012-12-14 10:49:50 +0000

    invite friend

--- app/code/local/Volo/Invitefriend/controllers/IndexController.php
+++ app/code/local/Volo/Invitefriend/controllers/IndexController.php
@@ -3,9 +3,24 @@
 class Volo_Invitefriend_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
-    {	
-		$this->loadLayout(array('default'));
-		$this->renderLayout();
+    {
+	if ( Mage::getSingleton('customer/session')->isLoggedIn())
+		{
+                $this->loadLayout(array('default'));
+                $this->renderLayout();
+        }
+        else
+        {
+                $this->_redirectUrl('/customer/account/login');
+        }	
     }
+
+	public function sendinviteAction()
+	{
+		$invite_email = $this->getRequest()->getParam('invite_email_0', false);
+		$response=array('result'=>$invite_email);
+		$response = Mage::helper('core')->jsonEncode($response);
+		$this->getResponse()->setBody($response);
+	}
 }
 ?>
