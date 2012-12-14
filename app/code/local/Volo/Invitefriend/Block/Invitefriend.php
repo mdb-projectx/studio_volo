b5301848e3218f3dbd6ea0254489ebe2d2559715
Edwin <edwin@welllinkhk.com>
2012-12-14 07:58:11 +0000

    Invite Friend

---
+++ app/code/local/Volo/Invitefriend/Block/Invitefriend.php
@@ -0,0 +1,12 @@
+<?php
+
+class Volo_Invitefriend_Block_Invitefriend extends Mage_Core_Block_Template
+{
+	 public function getCredit()
+    {
+		$result='1111';
+        return $result;
+    }
+
+}
+?>
