c6f25ba02e937c97fd4350beb13fe51b5089f6f6
Edwin <edwin@welllinkhk.com>
2012-12-17 09:40:12 +0000

    invite friend

--- app/code/local/Volo/Invitefriend/controllers/IndexController.php
+++ app/code/local/Volo/Invitefriend/controllers/IndexController.php
@@ -17,8 +17,36 @@ class Volo_Invitefriend_IndexController extends Mage_Core_Controller_Front_Actio
 
 	public function sendinviteAction()
 	{
-		$invite_email = $this->getRequest()->getParam('invite_email_0', false);
-		$response=array('result'=>$invite_email);
+		$result=true;
+		$emailcounter = $this->getRequest()->getParam('email_counter', false);
+		$invite_message = $this->getRequest()->getParam('invite_message', false);
+		for ($i=0; $i<=$emailcounter; $i++)
+		{
+			$email=$this->getRequest()->getParam('invite_email_'.$i, false);
+			if(filter_var($email, FILTER_VALIDATE_EMAIL))
+			{
+				$mail = Mage::getModel('core/email');
+                                $mail->setToEmail($email);
+                                $mail->setBody($invite_message);
+                                $mail->setSubject('Volodesign');
+                                $mail->setFromEmail('voloproject@gmail.com');
+                                $mail->setFromName('Volodesign');
+                                $mail->setType('html');// YOu can use Html or text as Mail format
+
+                                try {
+                                        $mail->send();
+                //Mage::getSingleton('core/session')->addSuccess('Your request has been sent');
+                                }
+                                catch (Exception $e) {
+                //Mage::getSingleton('core/session')->addError('Unable to send.');
+                                        $result=false;
+                                }
+
+			}
+		}
+
+//		$response=array('result'=>$invite_email);
+$response=array('result'=>$result);
 		$response = Mage::helper('core')->jsonEncode($response);
 		$this->getResponse()->setBody($response);
 	}
