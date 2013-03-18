4f7579e0fecafa067427a520b57c7fcbf72d7204
Edwin <edwin@welllinkhk.com>
2013-03-18 02:45:46 +0000

    invite splash

--- app/code/local/Volo/HelloWorld/controllers/IndexController.php
+++ app/code/local/Volo/HelloWorld/controllers/IndexController.php
@@ -4,7 +4,16 @@ class Volo_HelloWorld_IndexController extends Mage_Core_Controller_Front_Action
 {
     public function indexAction()
     {
-     $this->loadLayout(array('default'));
-     $this->renderLayout();
+	if ( Mage::getSingleton('customer/session')->isLoggedIn())
+        {
+		$this->loadLayout(array('default'));
+                $this->renderLayout();
+		//$this->_redirectUrl('/invite-friends-splash');
+        }
+        else
+        {
+                Mage::getSingleton('customer/session')->setBeforeAuthUrl(Mage::helper("core/url")->getCurrentUrl());
+                $this->_redirectUrl('/customer/account/login');
+        }
     }
 }
