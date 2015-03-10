3461d571513128aff64e3a39dd1a1826a57fbe21
Edwin <edwin@welllinkhk.com>
2015-03-10 18:43:07 +0000

    lazy load

--- skin/frontend/default/respond/js/volo.js
+++ skin/frontend/default/respond/js/volo.js
@@ -1,5 +1,65 @@
+/**
+ * jQuery Unveil
+ * A very lightweight jQuery plugin to lazy load images
+ * http://luis-almeida.github.com/unveil
+ *
+ * Licensed under the MIT license.
+ * Copyright 2013 Luís Almeida
+ * https://github.com/luis-almeida
+ */
+
+;(function($) {
+
+  $j.fn.unveil = function(threshold, callback) {
+
+    var $w = $j(window),
+        th = threshold || 0,
+        retina = window.devicePixelRatio > 1,
+        attrib = retina? "data-src-retina" : "data-src",
+        images = this,
+        loaded;
+
+    this.one("unveil", function() {
+      var source = this.getAttribute(attrib);
+      source = source || this.getAttribute("data-src");
+      if (source) {
+        this.setAttribute("src", source);
+        if (typeof callback === "function") callback.call(this);
+      }
+    });
+
+    function unveil() {
+      var inview = images.filter(function() {
+        var $e = $j(this);
+        if ($e.is(":hidden")) return;
+
+        var wt = $w.scrollTop(),
+            wb = wt + $w.height(),
+            et = $e.offset().top,
+            eb = et + $e.height();
+
+        return eb >= wt - th && et <= wb + th;
+      });
+
+      loaded = inview.trigger("unveil");
+      images = images.not(loaded);
+    }
+
+    $w.on("scroll.unveil resize.unveil lookup.unveil", unveil);
+
+    unveil();
+
+    return this;
+
+  };
+
+})(window.jQuery || window.Zepto);
+
+
 $j(document).ready(function() {
 
+	$j("img").unveil();
+	
 	var useragent = navigator.userAgent.toLowerCase();
 
 	if (useragent.indexOf("mobile") > -1)
@@ -363,3 +423,6 @@ $j(document).ready(function() {
 
 });
 
+$j(window).load(function() {
+	$j("img").trigger("unveil");
+});
