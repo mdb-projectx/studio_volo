24e695d62af7b9a87f047ab63013d6a3f6c0e97a
Edwin <edwin@welllinkhk.com>
2016-03-03 18:04:41 +0000

    payment extensions

---
+++ app/code/community/Cryozonic/Stripe/Block/Form/Standard.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Cryozonic
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Single Domain License
+ * that is available through the world-wide-web at this URL:
+ * http://cryozonic.com/licenses/stripe.html
+ * If you are unable to obtain it through the world-wide-web,
+ * please send an email to info@cryozonic.com so we can send
+ * you a copy immediately.
+ *
+ * @category   Cryozonic
+ * @package    Cryozonic_Stripe
+ * @copyright  Copyright (c) Cryozonic Ltd (http://cryozonic.com)
+ */
+
+class Cryozonic_Stripe_Block_Form_Standard extends Mage_Payment_Block_Form_Cc
+{
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setTemplate('cryozonic/stripe/form/standard.phtml');
+
+        // We need to check if Stripe Subscriptions is installed, this is the
+        // cross-magento version compatible way.
+        $path = dirname(__FILE__).DS.'..'.DS.'..'.DS.'Model'.DS.'Cryozonic_StripeSubscriptions.xml';
+        if (file_exists($path))
+            $this->stripe = Mage::getModel('cryozonic_stripe/subscriptions');
+        else
+            $this->stripe = Mage::getModel('cryozonic_stripe/standard');
+
+        $this->cardAutoDetect = $this->stripe->store->getConfig('payment/cryozonic_stripe/card_autodetect');
+    }
+
+    public function autoDetectCard()
+    {
+        return $this->cardAutoDetect && $this->cardAutoDetect > 0;
+    }
+
+    public function showAcceptedCardTypes()
+    {
+        return $this->cardAutoDetect == 1;
+    }
+
+    public function getOnCardNumberChangedAnimation()
+    {
+        switch ($this->cardAutoDetect)
+        {
+            case 1: return 'onCardNumberChangedFade';
+            case 2: return 'onCardNumberChangedSlide';
+            default: return '';
+        }
+    }
+
+    public function getOnKeyUpCardNumber()
+    {
+        if ($this->autoDetectCard())
+        {
+            $callback = $this->getOnCardNumberChangedAnimation();
+            return "onkeyup=\"$callback(this)\"";
+        }
+
+        return '';
+    }
+
+    public function getAcceptedCardTypes()
+    {
+        $types = Mage::getConfig()->getNode('global/payment/cryozonic_stripe/cc_types')->asArray();
+        $acceptedTypes = $this->stripe->store->getConfig('payment/cryozonic_stripe/cctypes');
+
+        uasort($types, array('Mage_Payment_Model_Config', 'compareCcTypes'));
+
+        foreach ($types as $data)
+        {
+            if (empty($acceptedTypes)) // Slide animation, returns all possible types
+            {
+                $cardTypes[$data['code']] = $data['name'];
+            }
+            else if (isset($data['code']) && isset($data['name']) && strstr($acceptedTypes, $data['code'])) // Fade animation, takes into account selected types
+            {
+                $cardTypes[$data['code']] = $data['name'];
+            }
+        }
+
+        return $cardTypes;
+    }
+}
--- skin/frontend/default/respond/capsule/js/burger.js
+++ skin/frontend/default/respond/capsule/js/burger.js
@@ -6,9 +6,12 @@
   var burgerContain = document.getElementsByClassName('b-container')[0];
   var burgerNav = document.getElementsByClassName('b-nav')[0];
 
+ if (typeof(burgerMenu) != 'undefined' && burgerMenu != null)
+                   {
   burgerMenu.addEventListener('click', function toggleClasses() {
     [body, burgerContain, burgerNav].forEach(function (el) {
       el.classList.toggle('open');
     });
   }, false);
+				   }
 })();
